749f45f7f46abe0ad5d8e034a64434f6824a8abd
Grant Spilsbury <gspilsbury@officeworks.com.au>
2019-08-24 11:53:17 +1000

    Removed logo

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,7 +7,6 @@ function App() {
     <div className="App">
       <Button>Hello</Button>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
